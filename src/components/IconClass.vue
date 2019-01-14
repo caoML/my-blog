2b497e771465d2471d09875d7d299065084320ad
caominglei <972121471@qq.com>
2019-01-14 10:52:25 +0800

    使用脚手架搭建模板

---
+++ src/components/IconClass.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg :class="svgName" aria-hidden="true">
+        <use :xlink:href="iconName"></use>
+        <!-- <use :xlink:href="iconName"></use> -->
+    </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    svgName: {
+      type: String,
+      default: 'svg-name'
+    }
+  },
+  computed: {
+    iconName () {
+      return `#icon-${this.iconClass}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .svg-name{
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="home">
+        <div class="header" :style="{background:'url('+selectBg+')'}">
+        <!-- <div class="header"> -->
+            <div class="header-title">
+                <p>hello</p>
+            </div>
+            <div class="eyes" @click="handleClick">
+                <svg-icon value=1 icon-class="browse" class="theme"></svg-icon>
+                <svg-icon value=2 icon-class="browse" class="theme"></svg-icon>
+                <svg-icon value=3 icon-class="browse" class="theme"></svg-icon>
+                <svg-icon value=4 icon-class="browse" class="theme"></svg-icon>
+                <svg-icon value=5 icon-class="browse" class="theme"></svg-icon>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bg: ['blue.jpg', 'green.jpg', 'pink.jpg', 'yellow.jpg'],
+      selectBg: '@/assets/img/blue.jpg'
+    //   ts: {backgroundImg: "url('+../../assets/img/blue.jpg+')"}
+      //   ts: {backgroundColor: 'blue'}
+    }
+  },
+  methods: {
+    handleClick (event) {
+      let value = event.target.getAttribute('value')
+      console.log(value)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+    .home{
+        .header{
+            position:relative;
+            // background:url('../../assets/img/blue.jpg');
+            height:500px;
+            .eyes{
+                .theme{
+                    transition: all 1s;
+                    color:white;
+                    width:1.5em;
+                    margin:0.5em;
+                    border-radius:20px;
+                    cursor:pointer;
+                }
+                .theme:hover{
+                    transition: all 1s;
+                    background-color:rgba(255,255,255,.45);
+                    box-shadow: 0 0 7px 1px #fff;
+                }
+            }
+        }
+    }
+</style>
